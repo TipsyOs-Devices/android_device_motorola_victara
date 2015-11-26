20cf166ac19c1289d81947b2b38c8ebe34033ba0
Alx31 <alex31.couderc@gmail.com>
2015-11-26 15:51:30 +0100

    try 4.9 uber toolchainz

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -54,8 +54,10 @@ TARGET_KERNEL_SOURCE := kernel/motorola/msm8974
 TARGET_KERNEL_CONFIG := cyanogenmod_victara_defconfig
 
 # Kernel Toolchain
-KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.9/bin
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.9-uber/bin
 KERNEL_TOOLCHAIN_PREFIX := arm-eabi-
+# Rom Toolchain
+TARGET_GCC_VERSION_EXP := 4.9-uber
 
 # Audio
 AUDIO_FEATURE_ENABLED_ANC_HEADSET := true
